d211b7c037c76d243d0e0322d384939abddd59c1
DRSWOLE5700 <98053692+DRSWOLE5700@users.noreply.github.com>
2022-05-07 10:48:37 -0700

    Cleaned up code, proper comments made

--- ArduinoInput.py
+++ ArduinoInput.py
@@ -1,27 +1,36 @@
+#Copyright 2022 Caleb Stupin
+#PHSC 480 Advanced Physics Laboratory
+#FILE NAME: ArduinoInput.py
+#PROJECT NAME: Raspberry Pi Oscilloscope with Fourier Analysis Capability
+#DESCRIPTION: Inputs values from the Arduino
+
 import numpy as np
 import time
 import serial
 
+#Initialize serial port (USB)
 ser = serial.Serial('/dev/ttyACM0', 38400, timeout = 1)
-t=1
 
+#For testing purposes
+#t=1
 #def oscilloscope_input():
 #    return np.sin(20*(t + time.time())) 
 #l = []
 
-old_L = ''
-
-lastVal = 0
-
+#FUNCTION: oscilloscope_input()
+#DESCRIPTION: returns an analog value from the Arduino ADC
+#PARAMETERS: N/A
+#RETURNS: output, a string the ADC value reading
+#GLOBALS: ser, the serial port information
 def oscilloscope_input():
-    input = str(ser.read()).strip('b\'')   #strips the string of b and ' characters 
+    input = str(ser.read()).strip('b\'')        #strips the string of b and ' characters 
     #print(ser.read(5))
     #str(input).strip('b\'')
     l = []
-    if input == '\\n':
+    if input == '\\n':                          #newline character signals start of the number
         
         newval = str(ser.read()).strip('b\'')
-        if newval != '\r':
+        if newval != '\r':                      #\r character signals end of number
             l.append(newval)
                 
         newval = str(ser.read()).strip('b\'')
@@ -39,7 +48,7 @@ def oscilloscope_input():
     output = ''.join(l)
     output.strip('\n')
     
-    return output.strip(' ')
+    return output.strip(' ')                    #newline characters and spaces will make isdigit() return false, so they must be stripped
     
 
 
